60f9c0c0d7607ffee7cd4244e018d660bca3063e
Yeferson Rosales <yefersonj.rr@gmail.com>
2024-05-27 07:23:41 -0400

    se agregan componente footer y navbar para mejorar la escalabilidad

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="bg-dark text-white mt-5">
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-md-4">
+          <h5>Sobre nosotros</h5>
+          <p>Conteo de Personas y Vehículos,
+            Control de Ocupación Máxima,
+            Gestión de Visitantes y
+            Estadísticas del Flujo.
+          </p>
+        </div>
+        <div class="col-md-4">
+          <h5>Links rápidos</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">Home</a></li>
+            <li><a href="https://www.linkedin.com/in/yeferson-rosales-r-4a37aa15a/" class="text-white" target="_blank">LinkedIn</a></li>
+            <li><a href="https://github.com/Yeyref" class="text-white" target="_blank">GitHub</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h5>Contáctame</h5>
+          <address>
+            Región Metropolitana<br>
+            Santiago de Chile<br>
+            Email: <a href="mailto:yefersonj.rr@gmail.com" class="text-white" target="_blank">yefersonj.rr@gmail.com</a><br>
+            Teléfono: +56 9 87216115
+          </address>
+        </div>
+      </div>
+      <div class="text-center mt-3">
+        <p>&copy; 2024 Yeferson Rosales Dev. Todos los derechos reservados.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
